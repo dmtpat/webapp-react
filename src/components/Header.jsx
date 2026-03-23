309ea4294ba5187a950f893a308eeadac2c65294
Pat <dmtpatricica@email.com>
2026-03-23 12:56:58 +0100

    Header component + style

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+function Header() {
+    return (
+        <header className="header">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/otherPage">Other</NavLink>
+        </header>
+    )
+}
+export default Header
